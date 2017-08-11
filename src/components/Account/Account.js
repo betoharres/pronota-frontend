16c82e7435973b0cc05fa67cb591cc983c55f45e
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-08-10 22:49:07 -0300

    Clear Account component

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
-import Paper from 'material-ui/Paper'
-import Divider from 'material-ui/Divider'
 
 import './styles.css'
 
@@ -9,14 +7,8 @@ export default function Account (props) {
 
   return (
     <div className='accountContainer'>
-      <Paper style={{padding: 20, margin: 20}} zDepth={1}>
-        <h1>Atualizar Senha</h1>
-        <br />
-        <Divider style={{marginTop: 40, margin: 20}} />
-        <div style={{alignItems: 'center'}}>
-          <RaisedButton onClick={props.onLogout} label='Logout' />
-        </div>
-      </Paper>
+      <br />
+      <RaisedButton onClick={props.onLogout} label='Logout' />
     </div>
   )
 
