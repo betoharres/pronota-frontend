d28e1ecd73c5147f88d174f675abd066bea90f51
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-12-05 16:44:50 -0200

    Makes API_DOMAIN configurable through ENV vars

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const API_PROTOCOL = 'http'
-export const API_DOMAIN = 'f7starter.dev'
+export const API_DOMAIN = process.env.REACT_APP_DOMAIN
 export const headers = {
   'Accept': 'application/vnd.mycompany+json;version=1',
   'Content-Type': 'application/json',
