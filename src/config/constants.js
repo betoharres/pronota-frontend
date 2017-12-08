fab1dd10d8ec3aa99cc31e6c0bd9ae70cabbe16e
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-12-08 15:42:06 -0200

    Add ENV var to API_PROTOCOL constant

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_PROTOCOL = 'http'
+export const API_PROTOCOL = process.env.REACT_APP_PROTOCOL
 export const API_DOMAIN = process.env.REACT_APP_DOMAIN
 export const headers = {
   'Accept': 'application/vnd.mycompany+json;version=1',
