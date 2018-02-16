6a5dcb73696a4fe69335676e1600d0a981f69d67
Alberto Harres Rocha <albertohrocha@gmail.com>
2018-02-16 11:10:43 -0200

    Add validations to Login and Register forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,11 @@ import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 import { TextForm } from '../../components/FormComponents'
 import { reduxForm, Field } from 'redux-form/immutable'
+import { email, required, minLength } from '../../validations'
 
 import './styles.css'
+
+const minLength8 = minLength(8)
 
 function Login (props) {
 
@@ -11,8 +14,8 @@ function Login (props) {
     <div>
       <h1 className='center'>Login</h1>
       <form onSubmit={props.handleSubmit}>
-        <Field name='email' label='E-mail' component={TextForm} />
-        <Field name='password' label='Senha' type="password" component={TextForm} />
+        <Field name='email' label='E-mail' component={TextForm} validate={[email, required]} />
+        <Field name='password' label='Senha' type="password" component={TextForm} validate={[required, minLength8]}/>
         <RaisedButton type='submit' label={'Entrar'} fullWidth={true}
           disabled={props.pristine || props.submitting} />
       </form>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,8 +2,11 @@ import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 import { TextForm } from '../../components/FormComponents'
 import { reduxForm, Field } from 'redux-form/immutable'
+import { email, required, minLength } from '../../validations'
 
 import './styles.css'
+
+const minLength8 = minLength(8)
 
 function Register (props) {
 
@@ -11,9 +14,11 @@ function Register (props) {
     <div>
       <h1 className='center'>Registre-se</h1>
       <form onSubmit={props.handleSubmit}>
-          <Field name='email' label='E-mail' component={TextForm} />
-          <Field name='password' label='Senha' type="password" component={TextForm} />
-          <Field name='password_confirmation' label='Confirme a Senha' type="password" component={TextForm} />
+          <Field name='email' label='E-mail' component={TextForm} validate={[required, email]}/>
+          <Field name='password' label='Senha' type="password" component={TextForm}
+            validate={[required, minLength8]}/>
+          <Field name='password_confirmation' label='Confirme a Senha' type="password" component={TextForm}
+            validate={[required, minLength8]}/>
           <RaisedButton type='submit' label={'Entrar'} fullWidth={true}
             primary={true} disabled={props.pristine || props.submitting} />
       </form>
